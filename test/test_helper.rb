98366b15229086d9331387da0fd31c49cf0fa540
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-11 12:13:20 +1000

    revert mocha load changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,25 @@ require File.dirname(__FILE__) + '/../lib/rubigen'
 require 'rubigen/helpers/generator_test_helper'
 include RubiGen
 
-begin
-  require 'mocha'
-rescue LoadError
+def load_mocha
   require 'rubygems'
-  gem 'mocha'
+  begin
+    gem 'mocha'
+  rescue LoadError
+    puts <<-EOS
+#{$!}
+
+This RubyGem is required to run the tests.
+
+Install: gem install mocha    
+EOS
+  end
   require 'mocha'
+end
+begin
+  gem 'mocha'
+rescue LoadError
+  load_mocha
+rescue NoMethodError
+  load_mocha
 end
