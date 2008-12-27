b78485be528592a37a20c1542feba73efdad147b
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-27 14:09:46 +1000

    don't reload options.rb if base.rb already loaded

--- lib/rubigen/scripts.rb
+++ lib/rubigen/scripts.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/options'
+require File.dirname(__FILE__) + '/options' unless Object.const_defined?("RubiGen") && RubiGen.const_defined?("Base")
 
 module RubiGen
   module Scripts
