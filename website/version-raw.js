8785fe1fa02b49efcb7742cfceef121fc6877b14
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-29 08:13:47 +1000

    temporarily removing website whilst moving to gh-pages branch

--- website/version-raw.js
+++
@@ -1,3 +0,0 @@
-// Announcement JS file
-var version = "1.3.2";
-MagicAnnouncement.show('compositekeys', version);
--- website/version.js
+++
@@ -1,4 +0,0 @@
-// Version JS file
-var version = "1.3.2";
-
-document.write(" - " + version);
