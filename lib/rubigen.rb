24b7ae1f71dd01ea493cb4485478c52978a49039
Christo DeLange <github@dldinternet.com>
2013-07-17 22:39:06 -0400

    Require version so RubiGen::VERSION is always set

--- lib/rubigen.rb
+++ lib/rubigen.rb
@@ -1,6 +1,8 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'rubigen/version'
+
 require 'active_support/all' 
 
 require 'rubigen/base'
